7c5a5b26f3268a9bdc30be321fecbb608d430954
QuangMinhK5 <minhdragonmcgamer@gmail.com>
2026-04-18 18:37:40 +0700

    Delete java/com/example/quiz directory

--- java/com/example/quiz/repository/QuizRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.quiz.repository;
-
-import com.example.quiz.entity.Quiz;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface QuizRepository extends JpaRepository<Quiz, Long> {
-    List<Quiz> findByUser_UserId(Long user_id);
-}
